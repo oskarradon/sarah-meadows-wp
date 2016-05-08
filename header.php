8683e4a414a456f06db43d743b8639b8654323c2
Oskar <oskarradonkimball@gmail.com>
2016-05-07 20:19:50 -0700

    Add content to header.php file
    responsive meta tag, opening html tag, dynamic wp_head , header logo

--- header.php
+++ header.php
@@ -1,3 +1,18 @@
-<p>
-  Header
-</p>
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php wp_title(); ?></title>
+
+  <?php wp_head(); ?>
+
+</head>
+
+<body>
+  <header>
+    <a href="<?php bloginfo('url'); ?>">
+      <h1>SARAH</h1>
+      <h1>MEADOWS</h1>
+    </a>
+  </header>
