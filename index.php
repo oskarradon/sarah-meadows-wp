452a76b1cccf5369dc54b36b88b68416e44c17fa
Oskar <oskarradonkimball@gmail.com>
2016-05-08 10:15:51 -0700

    Theoretical post location in loop

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php get_header(); ?>
-<h1>Index file</h1>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+  <div class="post">
+    <a href="<?php the_permalink(); ?>">
+      <img src="" alt="" />
+      <p><?php the_title(); ?></p>
+    </a>
+  </div>
+
+<?php endwhile; else : ?>
+	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
+
 <?php get_footer(); ?>
