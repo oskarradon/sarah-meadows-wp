fe1b152b2f5b847c820fb5f3f453b3775bcbe52c
Oskar <oskarradonkimball@gmail.com>
2016-05-31 10:22:08 -0700

    Comment out sidebar widgets, links to foundation files

--- functions.php
+++ functions.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 add_theme_support( 'menus' );
 add_theme_support( 'post-thumbnails' );
@@ -12,7 +12,7 @@ function register_theme_menus() {
 
 	register_nav_menus(
 		array(
-			'primary-menu' 	=> __( 'Primary Menu', 'treehouse-portfolio' )			
+			'primary-menu' 	=> __( 'Primary Menu', 'sarah-meadows' )
 		)
 	);
 
@@ -20,28 +20,28 @@ function register_theme_menus() {
 add_action( 'init', 'register_theme_menus' );
 
 
-function wpt_create_widget( $name, $id, $description ) {
-
-	register_sidebar(array(
-		'name' => __( $name ),	 
-		'id' => $id, 
-		'description' => __( $description ),
-		'before_widget' => '<div class="widget">',
-		'after_widget' => '</div>',
-		'before_title' => '<h2 class="module-heading">',
-		'after_title' => '</h2>'
-	));
-
-}
-
-wpt_create_widget( 'Page Sidebar', 'page', 'Displays on the side of pages with a sidebar' );
-wpt_create_widget( 'Blog Sidebar', 'blog', 'Displays on the side of pages in the blog section' );
+// function wpt_create_widget( $name, $id, $description ) {
+//
+// 	register_sidebar(array(
+// 		'name' => __( $name ),
+// 		'id' => $id,
+// 		'description' => __( $description ),
+// 		'before_widget' => '<div class="widget">',
+// 		'after_widget' => '</div>',
+// 		'before_title' => '<h2 class="module-heading">',
+// 		'after_title' => '</h2>'
+// 	));
+//
+// }
+//
+// wpt_create_widget( 'Page Sidebar', 'page', 'Displays on the side of pages with a sidebar' );
+// wpt_create_widget( 'Blog Sidebar', 'blog', 'Displays on the side of pages in the blog section' );
 
 
 function wpt_theme_styles() {
 
-	wp_enqueue_style( 'foundation_css', get_template_directory_uri() . '/css/foundation.css' );
-	//wp_enqueue_style( 'normalize_css', get_template_directory_uri() . '/css/normalize.css' );
+	// wp_enqueue_style( 'foundation_css', get_template_directory_uri() . '/css/foundation.css' );
+	wp_enqueue_style( 'normalize_css', get_template_directory_uri() . '/css/normalize.css' );
 	wp_enqueue_style( 'googlefont_css', 'http://fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic' );
 	wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
 
@@ -50,9 +50,9 @@ add_action( 'wp_enqueue_scripts', 'wpt_theme_styles' );
 
 function wpt_theme_js() {
 
-	wp_enqueue_script( 'modernizr_js', get_template_directory_uri() . '/js/modernizr.js', '', '', false );	
-	wp_enqueue_script( 'foundation_js', get_template_directory_uri() . '/js/foundation.min.js', array('jquery'), '', true );
-	wp_enqueue_script( 'main_js', get_template_directory_uri() . '/js/app.js', array('jquery', 'foundation_js'), '', true );		
+	wp_enqueue_script( 'modernizr_js', get_template_directory_uri() . '/js/modernizr.js', '', '', false );
+	// wp_enqueue_script( 'foundation_js', get_template_directory_uri() . '/js/foundation.min.js', array('jquery'), '', true );
+	wp_enqueue_script( 'main_js', get_template_directory_uri() . '/js/app.js', array('jquery'), '', true );
 
 }
 add_action( 'wp_enqueue_scripts', 'wpt_theme_js' );
