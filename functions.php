9a38f67860daaf85a716b60b8c5eaa4e0e8ea01b
Oskar <oskarradonkimball@gmail.com>
2016-05-31 10:29:35 -0700

    Add link to Roboto Mono Google font

--- functions.php
+++ functions.php
@@ -42,9 +42,8 @@ function wpt_theme_styles() {
 
 	// wp_enqueue_style( 'foundation_css', get_template_directory_uri() . '/css/foundation.css' );
 	wp_enqueue_style( 'normalize_css', get_template_directory_uri() . '/css/normalize.css' );
-	wp_enqueue_style( 'googlefont_css', 'http://fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic' );
+	wp_enqueue_style( 'googlefont_css', 'https://fonts.googleapis.com/css?family=Roboto+Mono:500,700,700italic,500italic' );
 	wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
-
 }
 add_action( 'wp_enqueue_scripts', 'wpt_theme_styles' );
 
