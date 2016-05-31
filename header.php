04c34a97ab677ff05c967e76849e7b1dfce6dade
Oskar <oskarradonkimball@gmail.com>
2016-05-31 10:44:08 -0700

    Remove toggle-able menu, remove foundation classes, get header looking REAL good

--- header.php
+++ header.php
@@ -10,42 +10,21 @@
   </head>
 
   <body <?php  body_class(); ?>>
-    <header class="row no-max pad main">
-  <h1><a class='current' href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
-  <a href="" class="nav-toggle"><span></span>Menu</a>
-  <nav>
-    <h1 class="open"><a class='current' href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
-      
-    <?php 
+    <header>
+      <nav>
+        <h1><a class='current' href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
 
-      $defaults = array(
-        'container' => false,
-        'theme_location'  => 'primary-menu',
-        'menu_class'  => 'no-bullet'
-      );
+        <?php
 
-      wp_nav_menu( $defaults );
+        $defaults = array(
+          'container' => false,
+          'theme_location'  => 'primary-menu',
+          'menu_class'  => 'no-bullet'
+        );
 
-    ?>
-<!--
-    <ul class="no-bullet">
-      <li class="current parent"><a class='current' href="index.html">Portfolio</a>
-        <ul class="sub-menu">
-          <li><a href="item.html">Portfolio Item</a></li>
-          <li><a href="item.html">Portfolio Item</a></li>
-          <li><a href="item.html">Portfolio Item</a></li>
-          <li><a href="item.html">Portfolio Item</a></li>
-        </ul>
-      </li>
-      <li class="parent"><a href="blog.html">Blog</a>
-        <ul class="sub-menu">
-          <li><a href="single-post.html">Single Post</a></li>
-          <li><a href="author.html">Author Page</a></li>
-        </ul>
-      </li>
-      <li><a href="about.html">About</a></li>
-      <li><a href="contact.html">Contact</a></li>
-    </ul>
--->
-  </nav>
-</header>
+        wp_nav_menu( $defaults );
+
+        ?>
+
+      </nav>
+    </header>
