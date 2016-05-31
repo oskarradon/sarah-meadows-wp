000c45ccc820904dc9f1b615be352371a08757a6
Oskar <oskarradonkimball@gmail.com>
2016-05-31 16:03:13 -0700

    Remove foundation classes and comment out footer copyright info

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
-    <div class="footer-clear"></div>
-<footer class="row no-max pad">           
-  <p>Copyright <?php echo date('Y'); ?></p>
+
+<footer>
+  <!-- <p>Copyright <?php echo date('Y'); ?></p> -->
 </footer>
 
   <?php wp_footer(); ?>
