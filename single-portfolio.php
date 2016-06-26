e37de3ae5876f1ddf323f8177318266754d9f9f2
Oskar <oskarradonkimball@gmail.com>
2016-06-26 14:09:56 -0700

    Images showing on portolio page

--- single-portfolio.php
+++ single-portfolio.php
@@ -5,9 +5,26 @@
 
 <h1 class="portfolio-title"><?php the_title(); ?></h1>
 
-	<?php the_field('images'); ?>
+<?php 
+
+$images = get_field('images');
+
+if( $images ): ?>
+    <ul>
+        <?php foreach( $images as $image ): ?>
+            <li>
+                <a href="<?php echo $image['url']; ?>">
+                     <img src="<?php echo $image['sizes']['thumbnail']; ?>" alt="<?php echo $image['alt']; ?>" />
+                </a>
+                <p><?php echo $image['caption']; ?></p>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
 
 <?php endwhile; endif; ?>
+
+
 
 
 <?php get_footer(); ?>
