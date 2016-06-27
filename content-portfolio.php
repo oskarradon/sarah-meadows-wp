2885b9498ebe4d1cecddb3faa275d4a305fc4a0d
Oskar <oskarradonkimball@gmail.com>
2016-06-27 15:45:00 -0700

    Change to using the main HTML tag instead of a section

--- content-portfolio.php
+++ content-portfolio.php
@@ -7,19 +7,21 @@
     'posts_per_page' => $num_posts
   );
   $query = new WP_Query( $args );
-
+  $unique_id = 0;
 ?>
 
 
-<section class="main-content">
+<main>
 
   <?php if( $query->have_posts() ) : while( $query->have_posts() ) : $query->the_post(); ?>
-
-  <div class="portfolio-item">
+  
+  <div id="post<?php echo $unique_id;?>" class="portfolio-item">
     <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium'); ?></a>
     <p class="post-title"><?php the_title(); ?></p>
   </div>
 
+  <?php $unique_id++; ?>
+  
   <?php endwhile; endif; wp_reset_postdata(); ?>
 
-</section>
+</main>
