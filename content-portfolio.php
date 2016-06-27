167cf622ca3ec6f1b332e35560f06dd6afeaf206
Oskar <oskarradonkimball@gmail.com>
2016-06-27 15:14:29 -0700

    Show post title and give it a class

--- content-portfolio.php
+++ content-portfolio.php
@@ -16,7 +16,8 @@
   <?php if( $query->have_posts() ) : while( $query->have_posts() ) : $query->the_post(); ?>
 
   <div class="portfolio-item">
-    <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('large'); ?></a>
+    <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium'); ?></a>
+    <p class="post-title"><?php the_title(); ?></p>
   </div>
 
   <?php endwhile; endif; wp_reset_postdata(); ?>
