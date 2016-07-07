c19e66d91ae0ff284c8e66fbc2d67f067c6a9e67
Oskar <oskarradonkimball@gmail.com>
2016-07-06 21:52:16 -0700

    Add toggle functionality to images in portfolio piece page

--- js/app.js
+++ js/app.js
@@ -1,15 +1,31 @@
 var $ = jQuery;
 
 $(function() {
-  if ( $('body').hasClass('home') ) {
-    if ( $('#post6').attr('id') === undefined ) {
-      $('body').css({ 'height' : '1000px' });
-    } else if ( $('#post11').attr('id') === undefined ) {
-      $('body').css({ 'height' : '1650px' });
-    } else if ( $('#post16').attr('id') === undefined ) {
-      $('body').css({ 'height' : '2200px' });
-    } else {
-      $('body').css({ 'height' : '3200px' });
-    }
+
+  if ( $('#post6').attr('id') === undefined ) {
+    $('body').css({ 'height' : '1000px' });
+  } else if ( $('#post11').attr('id') === undefined ) {
+    $('body').css({ 'height' : '1650px' });
+  } else if ( $('#post16').attr('id') === undefined ) {
+    $('body').css({ 'height' : '2200px' });
+  } else {
+    $('body').css({ 'height' : '3200px' });
+  }
+
+  var windowWidth = $(window).width();
+
+  $(window).resize(function() {
+    windowWidth = $(window).width();
+  
+  });
+
+  if ( windowWidth > 634 ) {
+    $('.portfolio-image').toggle(function() {
+      $(this).children('img').css({ 'width' : '400px' }).addClass('active');
+      $(this).children('.image-caption').css({ 'opacity' : 1 });
+    }, function () {
+      $(this).children('img').css({ 'width' : '250px' }).removeClass('active');
+      $(this).children('.image-caption').css({ 'opacity' : 0 });
+    })
   }
 });
