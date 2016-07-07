b68bfc808b323538a3ecb453a5c74f8f6cf4e8c3
Oskar <oskarradonkimball@gmail.com>
2016-07-06 21:51:40 -0700

    Add conditional statement to link to URL if post is of type 'link'

--- content-portfolio.php
+++ content-portfolio.php
@@ -3,7 +3,7 @@
   $num_posts = ( is_front_page() ) ? 20 : -1;
 
   $args = array(
-    'post_type' => 'portfolio',
+    'post_type' => array ( 'portfolio', 'link' ),
     'posts_per_page' => $num_posts
   );
   $query = new WP_Query( $args );
@@ -16,8 +16,21 @@
   <?php if( $query->have_posts() ) : while( $query->have_posts() ) : $query->the_post(); ?>
   
   <div id="post<?php echo $unique_id;?>" class="portfolio-item">
-    <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('post-size'); ?></a>
+
+    <?php if( get_post_type() == 'portfolio' ): ?>
+
+      <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('post-size'); ?></a>
+    
+    <?php endif; ?>
+
+    <?php if( get_post_type() == 'link' ): ?>
+
+      <a href="<?php echo get_field('link'); ?>"><?php the_post_thumbnail('post-size'); ?></a>
+    
+    <?php endif; ?>
+    
     <p class="post-title"><?php the_title(); ?></p>
+
   </div>
 
   <?php $unique_id++; ?>
