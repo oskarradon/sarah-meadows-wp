7af9a27e91924ad2a334ac08a0f266541dfde85f
Oskar <oskarradonkimball@gmail.com>
2016-07-12 16:57:10 -0700

    Remove title from single page template, wrap content in section

--- page.php
+++ page.php
@@ -4,8 +4,7 @@
 
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-      <h1><?php the_title(); ?></h1>
-      <p><?php the_content(); ?></p>
+      <section class="page-content"><?php the_content(); ?></section>
 
   	<?php endwhile; else : ?>
 
