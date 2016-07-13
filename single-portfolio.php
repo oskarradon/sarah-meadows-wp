caa039bd33cf9a8ed50d9ae5e80fa87583282b8e
Oskar <oskarradonkimball@gmail.com>
2016-07-12 17:12:47 -0700

    Make portfolio-title p not h1

--- single-portfolio.php
+++ single-portfolio.php
@@ -3,7 +3,7 @@
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-	<h1 class="portfolio-title"><?php the_title(); ?></h1>
+	<p class="portfolio-title"><?php the_title(); ?></p>
 
 	<section class="portfolio-gallery">
 
